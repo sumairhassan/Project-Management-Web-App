aba1296fa50ae7a4029240361605f3cf58674ddd
Sumair Hassan <sumairhassan10@gmail.com>
2021-01-23 22:29:24 +0100

    Implementing Redux

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,17 @@
+const initState = {
+  projects: [
+    { id: "1", title: "Title number 1", content: "Content of Title number 1" },
+    { id: "2", title: "Title number 2", content: "Content of Title number 2" },
+    { id: "3", title: "Title number 3", content: "Content of Title number 3" },
+  ],
+};
+
+const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT':
+      console.log('created project', action.project)
+  }
+  return state;
+};
+
+export default projectReducer;
